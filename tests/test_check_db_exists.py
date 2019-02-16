64f61339322533862bbf0f356ca8c6603ac15e39
Kirill Ivanov <ivanovkirillk@gmail.com>
2019-02-17 00:04:16 +0300

    telegraf config + testcase for existing db added

--- tests/test_check_db_exists.py
+++ tests/test_check_db_exists.py
@@ -17,3 +17,7 @@ def test_no_data_to_connect_case():
 
 def test_db_does_not_exist_case():
     assert check_db_exists('telegraf', 'localhost', '8086', 'telegraf', 'telegraf', logger) is False
+
+
+def test_db_does_exist_case():
+    assert check_db_exists('telegraf', 'localhost', '8090', 'telegraf', 'telegraf', logger) is True
