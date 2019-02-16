4c92e7821265110717777ec5e4942abcb4ef4f92
Kirill Ivanov <ivanovkirillk@gmail.com>
2019-02-16 19:42:07 +0300

    conftest added

--- tests/test_check_db_exists.py
+++ tests/test_check_db_exists.py
@@ -1,8 +1,6 @@
 from tasks import check_db_exists
 import pytest
-import sys, os
-myPath = os.path.dirname(os.path.abspath(__file__))
-sys.path.insert(0, myPath + '/../')
+
 
 
 def test_no_connection_case():
--- tests/test_check_file_exists.py
+++ tests/test_check_file_exists.py
@@ -1,8 +1,6 @@
 from tasks import check_file_exists
 import pytest
-import sys, os
-myPath = os.path.dirname(os.path.abspath(__file__))
-sys.path.insert(0, myPath + '/../')
+
 
 
 def test_check_file_exists_case():
