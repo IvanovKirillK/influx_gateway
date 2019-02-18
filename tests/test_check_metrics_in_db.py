8c623f3687ccc2b55897716a1f9e4eadff918f0b
Kirill Ivanov <ivanovkirillk@gmail.com>
2019-02-18 19:17:43 +0300

    test modification

--- tests/test_check_metrics_in_db.py
+++ tests/test_check_metrics_in_db.py
@@ -33,20 +33,17 @@ def test_no_connection_case():
 
 def test_no_measurements_in_db():
     metrics_list = ['cpu', 'disk']
-    #create_db()
     assert check_metrics_in_db(metrics_list, 'telegraf', '127.0.0.1', '8090', 'telegraf', 'telegraf', logger) \
            == ['cpu', 'disk']
 
 
 def test_some_measurements_not_found(create_db_with_measurments):
     metrics_list = ['cpu', 'disk', 'temp']
-    #create_db_with_measurments()
     assert check_metrics_in_db(metrics_list, 'telegraf', '127.0.0.1', '8090', 'telegraf', 'telegraf', logger) \
            == ['temp']
 
 
 def test_measurements_in_db(create_db_with_measurments):
     metrics_list = ['cpu', 'disk']
-    #create_db_with_measurments()
     assert check_metrics_in_db(metrics_list, 'telegraf', '127.0.0.1', '8090', 'telegraf', 'telegraf', logger) \
            is True
