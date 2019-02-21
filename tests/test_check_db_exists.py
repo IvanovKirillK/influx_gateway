97db448c0c8abc01547eee71ee12f27218902bbf
Kirill Ivanov <ivanovkirillk@gmail.com>
2019-02-21 13:20:16 +0300

    gitlab-ci test update

--- tests/test_check_db_exists.py
+++ tests/test_check_db_exists.py
@@ -1,23 +1,30 @@
 from tasks import check_db_exists
 import pytest
 import logging
-
+from influxdb import InfluxDBClient
 
 logger = logging.getLogger('testlog')
 
+@pytest.fixture()
+def create_db():
+    client = InfluxDBClient('127.0.0.1', '8090', 'telegraf', 'telegraf', 'telegraf')
+    client.create_database('telegraf')
+    yield client
+    client.drop_database('telegraf')
+
 
 def test_no_connection_case():
-    assert check_db_exists('telegraf', '127.0.0.1', '8080', 'telegraf', 'telegraf', logger) == 'No Connect'
+    assert check_db_exists('telegraf', 'influxdb_local', '8080', 'telegraf', 'telegraf', logger) == 'No Connect'
 
 
 def test_no_data_to_connect_case():
     with pytest.raises(AttributeError):
-        check_db_exists('telegraf', '127.0.0.1', None, 'telegraf', 'telegraf', None)
+        check_db_exists('telegraf', 'influxdb_local', None, 'telegraf', 'telegraf', None)
 
 
 def test_db_does_not_exist_case():
-    assert check_db_exists('telegraf', '127.0.0.1', '8086', 'telegraf', 'telegraf', logger) is False
+    assert check_db_exists('telegraf', 'influxdb_local', '8086', 'telegraf', 'telegraf', logger) is False
 
 
-def test_db_does_exist_case():
-    assert check_db_exists('test_db', '127.0.0.1', '8090', 'telegraf', 'telegraf', logger) is True
+def test_db_does_exist_case(create_db):
+    assert check_db_exists('test_db', 'influxdb_remote', '8090', 'telegraf', 'telegraf', logger) is True
--- tests/test_create_db.py
+++ tests/test_create_db.py
@@ -6,8 +6,8 @@ logger = logging.getLogger('testlog')
 
 
 def test_no_connection_case():
-    assert create_db('telegraf', '127.0.0.1', '8080', 'telegraf', 'telegraf', logger) == 'No Connect'
+    assert create_db('telegraf', 'influxdb_remote', '8080', 'telegraf', 'telegraf', logger) == 'No Connect'
 
 
 def test_create_db_case():
-    assert create_db('telegraf', '127.0.0.1', '8086', 'telegraf', 'telegraf', logger) is True
+    assert create_db('telegraf', 'influxdb_local', '8086', 'telegraf', 'telegraf', logger) is True
